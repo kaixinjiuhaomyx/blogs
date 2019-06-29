e5a8f5bb299ed29c547bbf90dea99ef0e7e9a3c3
kaixinjiuhaomyx <1416191825@qq.com>
2019-06-29 19:37:34 +0800

    express

---
+++ blogs/controllers/user.js
@@ -0,0 +1,11 @@
+// 注册
+exports.reg = function(req,res,next){
+    res,render("reg.ejs");
+}
+
+exports.do_reg = function(req,res,next){
+    var name = req.body.uname;
+    var pass = req.body.pass;
+    console.log(name);
+    console.log(pass);
+}
